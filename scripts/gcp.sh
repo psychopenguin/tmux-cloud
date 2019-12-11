4db4c25763f9e8bc592e38454ac86a9493ececae
Fabio Brito d'Araujo e Oliveira <psychopenguin@gmail.com>
2019-12-11 10:41:49 +0100

    refactor to use pure shell

--- scripts/gcp.sh
+++ scripts/gcp.sh
@@ -4,11 +4,11 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CLOUDSDK_PYTHON=python3
 
 source $CURRENT_DIR/helpers.sh
-active_cfg=$(gcloud config configurations list --filter="is_active=true" --format="value(name)")
-cfgget=$(gcloud config configurations describe $active_cfg --format="value(name,properties.core.account,properties.core.project,properties.compute.zone)")
-cfgname="$(echo $cfgget|awk '{print $1}')"
-icon="#[bg=colour76,fg=colour231]   $cfgname #[fg=colour76,bg=colour11]$separator#[default]"
-account="#[bg=colour11,fg=colour231]$(echo $cfgget|awk '{print $2}')#[fg=colour11,bg=colour196]$separator#[default]"
-project="#[bg=colour196,fg=colour231]$(echo $cfgget|awk '{print $3}')#[fg=colour196,bg=colour69]$separator#[default]"
-zone="#[bg=colour69,fg=colour231]$(echo $cfgget|awk '{print $4}')#[default]"
+GCLOUD_HOME=$HOME/.config/gcloud
+active_cfg=$(cat $GCLOUD_HOME/active_config)
+source <(grep =  ~/.config/gcloud/configurations/config_$active_cfg|sed 's/ *= */=/g;s/^/cfg_/g')
+icon="#[bg=colour76,fg=colour231]   $active_cfg #[fg=colour76,bg=colour11]$separator#[default]"
+account="#[bg=colour11,fg=colour231]${cfg_account:-none}#[fg=colour11,bg=colour196]$separator#[default]"
+project="#[bg=colour196,fg=colour231]${cfg_project:-none}#[fg=colour196,bg=colour69]$separator#[default]"
+zone="#[bg=colour69,fg=colour231]${cfg_zone:-none}#[default]"
 echo -n "$icon$account$project$zone"
